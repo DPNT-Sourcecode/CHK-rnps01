82838fcec21bcda40a63436fba807cecb27d7ea5
sbx_user1051 <sbx_user1051@169.254.147.125>
2023-06-03 11:44:18 +0000

    Sat Jun 03 11:44:18 UTC 2023

--- test/solution_tests/CHK/test_hello.py
+++ test/solution_tests/CHK/test_hello.py
@@ -6,10 +6,11 @@ class TestCheckout():
         assert checkout_solution.checkout("A B C A A") == 180
 
     def test_checkout_sum_2(self):
-        assert checkout_solution.checkout("A A A A B B B B B") == 405
+        assert checkout_solution.checkout("A A A A B B B B B") == 300
 
     def test_checkout_invalid(self):
         assert checkout_solution.checkout("A B C A A Z") == -1
+
 
 
 
